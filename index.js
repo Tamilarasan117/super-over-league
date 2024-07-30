e6f59b201e1fb07b3559fdf91b1d3859b575ac9b
Tamilarasan117 <tamilarasan31403@gmail.com>
2024-07-30 12:48:03 +0530

    reactjs commit

---
+++ index.js
@@ -0,0 +1,10 @@
+const element = (
+    <div className="container">
+        <h1 className="head">Super Over League</h1>
+        <div className="img-cont">
+            <img src="https://assets.ccbp.in/frontend/react-js/rcb-img.png" alt="RCB" className="img" />
+            <img src="https://assets.ccbp.in/frontend/react-js/csk-img.png" alt="CSK" className="img" />
+        </div>
+    </div>
+)
+ReactDOM.render(element,document.getElementById("root"))
